5c7a6550b6c6f895d8cf617fbc967634f939ab75
amirhoseyn221177 <amirsayuar221177@gmail.com>
2020-10-03 17:46:59 -0700

    finished the blockChain but havent tested it

--- BlockChain.js
+++ BlockChain.js
@@ -1,77 +1,139 @@
-const sha = require('sha256')
-const currentUrl=process.argv[3]
-const uuid=require('uuid');
+const sha = require("sha256");
+const currentUrl = process.argv[3];
+const uuid = require("uuid");
 
 class BlockChain {
-    constructor() {
-        this.chain = [];
-        this.pendingTransactions = [];
-        this.currentNodeUrl=currentUrl
-        this.netWorkNodes=[]
-        this.createNewBlock(100,'0','0')
+  constructor() {
+    this.chain = [];
+    this.pendingTransactions = [];
+    this.currentNodeUrl = currentUrl;
+    this.netWorkNodes = [];
+    this.createNewBlock(100, "0", "0");
+  }
+  createNewBlock(nonce, previousBlockHash, hash) {
+    const newBlock = {
+      index: this.chain.length + 1,
+      timestamp: Date.now(),
+      transactions: this.pendingTransactions,
+      nonce: nonce,
+      hash: hash,
+      previousBlockHash: previousBlockHash,
+    };
+
+    this.pendingTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
+  }
+  getLastBlock() {
+    console.log(this.chain);
+    return this.chain[this.chain.length - 1];
+  }
+
+  createNewTransaction(amount, sender, recipient) {
+    const newTransaction = {
+      Id: uuid.v1().split("-").join(""),
+      amount: amount,
+      sender: sender,
+      recipient: recipient,
+    };
+    // this.pendingTransactions.push(newTransaction);
+    return newTransaction;
+  }
+
+  addToPending(transaction) {
+    console.log(transaction);
+    this.pendingTransactions.push(transaction);
+    return this.getLastBlock["index"] + 1;
+  }
+
+  hashBlock(prevBlock, currentBlock, nonce) {
+    const dataString =
+      prevBlock + nonce.toString() + JSON.stringify(currentBlock);
+    const hash = sha(dataString);
+    return hash;
+  }
+  proofOfWork(prevHash, currentHash) {
+    let nonce = 0;
+    let hash = this.hashBlock(prevHash, currentHash, nonce);
+
+    while (hash.substring(0, 4) !== "0000") {
+      nonce++;
+      hash = this.hashBlock(prevHash, currentHash, nonce);
     }
-    createNewBlock(nonce, previousBlockHash, hash) {
-        const newBlock = {
-            index: this.chain.length + 1,
-            timestamp: Date.now(),
-            transactions: this.pendingTransactions,
-            nonce: nonce,
-            hash: hash,
-            previousBlockHash: previousBlockHash
-        };
 
-        this.pendingTransactions = [];
-        this.chain.push(newBlock);
-
-        return newBlock;
+    return nonce;
+  }
+  chainIsValid(blockChain) {
+    let validChain = true;
+    for (let i = 1; i < blockChain.length; i++) {
+      const currBlock = blockChain[i];
+      const prevBlock = blockChain[i - 1];
+      const blockHash = this.hashBlock(
+        prevBlock["hash"],
+        { transaction: currBlock["transactions"], index: currBlock["index"] },
+        currentBlock["nonce"]
+      );
+      if (blockHash.substring(0, 4) !== "0000") validChain = false;
+      if (currBlock["previousBlockHash"] !== prevBlock["hash"]) {
+        validChain = false;
+      }
     }
-    getLastBlock() {
-        console.log(this.chain);
-        return this.chain[this.chain.length - 1];
-    }
+    const genesisBlock = blockChain[0];
+    const correctNonce = genesisBlock["nonce"] === 100;
+    const correctPreviousHash = genesisBlock["previousBlockHash"] === "0";
+    const correctHash = genesisBlock["hash"] === "0";
+    const correctTransaction = genesisBlock["transactions"].length === 0;
+    if (
+      correctHash ||
+      correctNonce ||
+      correctPreviousHash ||
+      correctTransaction
+    )
+      validChain = false;
+    return validChain;
+  }
 
-    createNewTransaction(amount, sender, recipient) {
-        const newTransaction = {
-            Id:uuid.v1().split('-').join(''),
-            amount: amount,
-            sender: sender,
-            recipient: recipient
-        };
-        // this.pendingTransactions.push(newTransaction);
-        return newTransaction;
-    }
+  getBlock(hash) {
+    let correctBlock = null;
+    this.chain.forEach((b) => {
+      if (b.hash === hash) {
+        correctBlock = b;
+      }
+    });
+    return correctBlock;
+  }
 
-    addToPending(transaction){
-        console.log(transaction)
-        this.pendingTransactions.push(transaction)
-        return this.getLastBlock['index']+1
-    }
-
-
-    hashBlock(prevBlock, currentBlock, nonce) {
-        const dataString = prevBlock + nonce.toString() + JSON.stringify(currentBlock);
-        const hash = sha(dataString);
-        return hash;
-    }
-    proofOfWork(prevHash, currentHash) {
-        let nonce = 0;
-        let hash = this.hashBlock(prevHash, currentHash, nonce);
-
-        while (hash.substring(0, 4) !== '0000') {
-            nonce++;
-            hash = this.hashBlock(prevHash, currentHash, nonce);
+  getTransaction(transactionId) {
+    let correctTransaction = null;
+    let correctBlock = null;
+    this.chain.forEach((b) => {
+      b.pendingTransactions.forEach((t) => {
+        if (t.transactionId === transactionId) {
+          correctTransaction = t;
+          correctBlock = b;
         }
+      });
+    });
+    return { block: correctBlock, transaction: correctTransaction };
+  }
 
-        return nonce;
-    }
-    chainIsValid(blockChain){
-        for(let i=1;i<blockChain.length;i++){
-            const currBlock=blockChain[i]
-            const prevBlock=blockChain[i-1]
-            if()
-        }
-    }
+  getAddress(address) {
+    const addressTransactions = [];
+    this.chain.forEach((b) => {
+      b.forEach((t) => {
+        if (transaction.sender === address || transaction.recipient === address)
+          addressTransactions.push(t);
+      });
+    });
+    let balance = 0;
+    addressTransactions.forEach((t) => {
+      if (t.recipient === address) balance += t.amount;
+      else if (t.sender === address) balance -= t.amount;
+    });
+
+    return { transaction: addressTransactions, balance: balance };
+  }
 }
 
-
-module.exports=BlockChain
+module.exports = BlockChain;
--- routes/NetworkNode.js
+++ routes/NetworkNode.js
@@ -1,110 +1,212 @@
-const express=require('express')
-const router= express.Router();
-const BlockChain=require('../BlockChain')
-const uuid=require('uuid');
-const bitcoin=new BlockChain()
-const nodeAdress=uuid.v1().split('-').join('')
-const { default: Axios } = require('axios');
+const express = require("express");
+const router = express.Router();
+const BlockChain = require("../BlockChain");
+const uuid = require("uuid");
+const bitcoin = new BlockChain();
+const nodeAdress = uuid.v1().split("-").join("");
+const { default: Axios } = require("axios");
+const e = require("express");
 
-router.get('/blockchain',(req,res)=>{
-    res.send(bitcoin)
-})
+router.get("/blockchain", (req, res) => {
+  res.send(bitcoin);
+});
 
-router.post('/transaction',async(req,res)=>{
-    const newTransaction= await req.body
-   const index= bitcoin.addToPending(newTransaction)
-    // res.json({note:"has been added successfully to"+index})
-})
+router.post("/transaction", async (req, res) => {
+  const newTransaction = await req.body;
+  const index = bitcoin.addToPending(newTransaction);
+  // res.json({note:"has been added successfully to"+index})
+});
 
-router.post('/transaction/broadcast',async(req,res)=>{
-    console.log(20)
-    const transaction=await req.body
-    const createNewTransaction=bitcoin.createNewTransaction(transaction.amount,transaction.sender,transaction.recipient)
-    bitcoin.addToPending(createNewTransaction)
-    bitcoin.netWorkNodes.map(async(n)=>{
-        await Axios.post(n+'/api/transaction',createNewTransaction)
-    })
-    res.json({note:"its done"})
-})
+router.post("/transaction/broadcast", async (req, res) => {
+  console.log(20);
+  const transaction = await req.body;
+  const createNewTransaction = bitcoin.createNewTransaction(
+    transaction.amount,
+    transaction.sender,
+    transaction.recipient
+  );
+  bitcoin.addToPending(createNewTransaction);
+  bitcoin.netWorkNodes.map(async (n) => {
+    await Axios.post(n + "/api/transaction", createNewTransaction);
+  });
+  res.json({
+    note: "its done",
+  });
+});
 
+router.get("/mine", async (req, res) => {
+  const lastBlock = bitcoin.getLastBlock();
+  const previHash = lastBlock["hash"];
+  const currBlock = {
+    transactions: bitcoin.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
 
-router.get('/mine',async(req,res)=>{
-    const lastBlock=bitcoin.getLastBlock()
-    const previHash=lastBlock['hash']
-    const currBlock={
-        transactions:bitcoin.pendingTransactions,
-        index:lastBlock['index']+1
+  const nonce = bitcoin.proofOfWork(previHash, currBlock);
+  const blockHash = bitcoin.hashBlock(previHash, currBlock, nonce);
+  const newBlock = bitcoin.createNewBlock(nonce, previHash, blockHash);
+  console.log(newBlock);
+  bitcoin.netWorkNodes.map(async (n) => {
+    const resp = await Axios.post(n + "/api/receive-new-block", {
+      newBlock: newBlock,
+    });
+  });
+
+  const resp = await Axios.post(
+    bitcoin.currentNodeUrl + "/api/transaction/broadcast",
+    {
+      amount: 12,
+      sender: "00",
+      recipient: nodeAdress,
     }
+  );
+  console.log(resp.data);
+  res.json({
+    note: "new block",
+    block: newBlock,
+  });
+});
 
-    const nonce = bitcoin.proofOfWork(previHash,currBlock)
-    const blockHash=bitcoin.hashBlock(previHash,currBlock,nonce)
-    const newBlock= bitcoin.createNewBlock(nonce,previHash,blockHash)
-    console.log(newBlock)
-    bitcoin.netWorkNodes.map(async(n)=>{
-         const resp=await Axios.post(n+'/api/receive-new-block',{newBlock:newBlock})
-    })
+router.post("/receive-new-block", async (req, res) => {
+  console.log(53);
+  const newBlock = await req.body.newBlock;
+  const lastBlock = bitcoin.getLastBlock();
+  const correcthash = lastBlock.hash === newBlock.previousBlockHash;
+  const correctIndex = lastBlock["index"] + 1 === newBlock["index"];
+  if (correctIndex && correcthash) {
+    bitcoin.chain.push(newBlock);
+    bitcoin.pendingTransactions = [];
+    res.json({
+      note: "new block added ",
+    });
+  } else {
+    res.json({
+      note: "rejected block",
+    });
+  }
+});
 
-    const resp=await Axios.post(bitcoin.currentNodeUrl+'/api/transaction/broadcast',{amount:12,sender:'00',recipient:nodeAdress})
-    console.log(resp.data)
-    res.json({ note: "new block", block: newBlock })
-})
+router.post("/register-node-broadcast-node", async function (req, res) {
+  const newNodeUrl = await req.body.newNodeUrl;
+  if (
+    !bitcoin.netWorkNodes.includes(newNodeUrl) &&
+    bitcoin.currentNodeUrl !== newNodeUrl
+  ) {
+    console.log(newNodeUrl);
+    bitcoin.netWorkNodes.push(newNodeUrl);
+    let data = {
+      newNodeUrl: newNodeUrl,
+    };
+    bitcoin.netWorkNodes.map(async (n) => {
+      const resp = await Axios.post(n + "/api/register-node", data);
+      console.log(resp.data);
+    });
+    await Axios.post(newNodeUrl + "/api/register-node-bulk", {
+      allNodes: [...bitcoin.netWorkNodes, bitcoin.currentNodeUrl],
+    });
+    res.json({
+      message: "node has been registered",
+    });
+  } else
+    res.json({
+      note: "sorry its not possible",
+    });
+});
 
+router.post("/register-node", async (req, res) => {
+  const newNodeUrl = await req.body.newNodeUrl;
+  console.log("58" + newNodeUrl);
+  if (
+    !bitcoin.netWorkNodes.includes(newNodeUrl) &&
+    bitcoin.currentNodeUrl !== newNodeUrl
+  ) {
+    bitcoin.netWorkNodes.push(newNodeUrl);
+    return res.json({
+      message: "new node registered",
+    });
+  }
+  res.json({
+    message: "sorry we couldnot register  ",
+  });
+});
 
-router.post('/receive-new-block',async(req,res)=>{
-    console.log(53)
-    const newBlock= await req.body.newBlock
-    const lastBlock=bitcoin.getLastBlock()
-    const correcthash=lastBlock.hash===newBlock.previousBlockHash
-    const correctIndex=lastBlock['index']+1===newBlock['index']
-    if(correctIndex&&correcthash){
-        bitcoin.chain.push(newBlock)
-        bitcoin.pendingTransactions=[]
-        res.json({note:"new block added "})
-    }else{
-        res.json({note:"rejected block"})
+router.post("/register-node-bulk", async (req, res) => {
+  const allNodes = await req.body.allNodes;
+  console.log(allNodes);
+  allNodes.forEach((node) => {
+    if (
+      !bitcoin.netWorkNodes.includes(node) &&
+      bitcoin.currentNodeUrl !== node
+    ) {
+      bitcoin.netWorkNodes.push(node);
     }
-})
+  });
+  res.json({
+    meesage: "bulk registeration successful",
+  });
+});
 
+router.get("/consenus", async (req, res) => {
+  const allBlockChains = [];
+  bitcoin.netWorkNodes.map(async (n) => {
+    const resp = await Axios.get(n + "/api/blockchain");
+    const allData = await resp.data;
+    allBlockChains.push(allData);
+  });
 
-router.post('/register-node-broadcast-node',async function (req, res) {
-        const newNodeUrl = await req.body.newNodeUrl;
-        if (!bitcoin.netWorkNodes.includes(newNodeUrl)&&bitcoin.currentNodeUrl!==newNodeUrl){
-            console.log(newNodeUrl);
-            bitcoin.netWorkNodes.push(newNodeUrl);
-            let data = { newNodeUrl: newNodeUrl };
-            bitcoin.netWorkNodes.map(async(n)=>{
-                const resp= await Axios.post(n+'/api/register-node',data)
-                console.log(resp.data)
-            })
-         await Axios.post(newNodeUrl+'/api/register-node-bulk',{allNodes:[...bitcoin.netWorkNodes,bitcoin.currentNodeUrl]})
-         res.json({message:'node has been registered'})
-        }else(
-            res.json({note:'sorry its not possible'})
-        )
+  const currentChainLength = bitcoin.chain.length;
+  let maxChainLength = currentChainLength;
+  let newLongestChain = null;
+  let newPendingTransaction = null;
+  allBlockChains.map((block) => {
+    if (block.chain.length > maxChainLength) {
+      maxChainLength = block.chain.length;
+      newLongestChain = block.chain;
+      newPendingTransaction = block.pendingTransactions;
     }
-    
-)
+  });
+  if (
+    !newLongestChain ||
+    (newLongestChain && !bitcoin.chainIsValid(newLongestChain))
+  ) {
+    res.json({
+      note: "current chain has not been replaced",
+    });
+  } else if (newLongestChain & bitcoin.chainIsValid(newLongestChain)) {
+    bitcoin.chain = newLongestChain;
+    bitcoin.pendingTransactions = newPendingTransaction;
+    res.json({
+      note: "this chain has been replaced",
+    });
+  }
+});
 
-router.post('/register-node',async(req,res)=>{
-    const newNodeUrl= await req.body.newNodeUrl
-    console.log("58"+newNodeUrl)
-        if(!bitcoin.netWorkNodes.includes(newNodeUrl)&&bitcoin.currentNodeUrl!==newNodeUrl){
-            bitcoin.netWorkNodes.push(newNodeUrl)
-         return res.json({message:"new node registered"})
-        }
-         res.json({message:"sorry we couldnot register  "})
- 
-})
+router.get("/block/:blockhash", async (req, res) => {
+  const blockHash = req.params.blockHash;
+  const correctBlock = bitcoin.getBlock(blockHash);
+  if (correctBlock) {
+    res.json({
+      note: "this is the block",
+      block: correctBlock,
+    });
+  } else {
+    res.json({
+      note: "there is no such a block",
+    });
+  }
+});
 
-router.post('/register-node-bulk',async(req,res)=>{
-    const allNodes=await req.body.allNodes
-    console.log(allNodes)
-    allNodes.forEach(node=>{
-        if(!bitcoin.netWorkNodes.includes(node)&&bitcoin.currentNodeUrl!==node){
-            bitcoin.netWorkNodes.push(node)
-        }
-    })
-    res.json({meesage:"bulk registeration successful"})
-})
+router.get("/transaction/:transaction", async (req, res) => {
+  const currentTransactionId = req.params.transactionId;
+  const { block, transaction } = bitcoin.getTransaction(currentTransactionId);
+  res.json({ block: block, transaction: transaction });
+});
 
-module.exports=router
+router.get("/address/:address", async (req, res) => {
+  const address = req.params.address;
+  const addressData=bitcoin.getAddress(address)
+  res.json({addressData:addressData})
+});
+
+
+module.exports = router;
